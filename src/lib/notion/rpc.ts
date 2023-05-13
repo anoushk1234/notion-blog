5a91d401cd130551f9c6a5fc8a58f3e8d6104c09
anoushk1234 <anoushka1828@gmail.com>
2023-05-13 12:24:16 +0530

    recordMap

--- src/lib/notion/rpc.ts
+++ src/lib/notion/rpc.ts
@@ -1,7 +1,7 @@
 import fetch, { Response } from 'node-fetch'
 import { API_ENDPOINT, NOTION_TOKEN } from './server-constants'
 
-export default async function rpc(fnName: string, body: any) {
+export default async function rpc(fnName: string, body: any): Promise<any> {
   if (!NOTION_TOKEN) {
     throw new Error('NOTION_TOKEN is not set in env')
   }
